383467d5fee275a8015c3b588a0b0623bf9e3e4b
Minji <mju_min77@naver.com>
2017-05-20 16:06:34 +0900

    플롯팅버튼 생성

--- app/src/main/java/com/ebookfrenzy/tablayoutdemo/TabLayoutDemoActivity.java
+++ app/src/main/java/com/ebookfrenzy/tablayoutdemo/TabLayoutDemoActivity.java
@@ -3,6 +3,7 @@ package com.ebookfrenzy.tablayoutdemo;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
@@ -12,16 +13,39 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 import com.tsengvn.typekit.TypekitContextWrapper;
 
 public class TabLayoutDemoActivity extends AppCompatActivity {
+
+    //플롯팅버튼 변수
+    Animation FabOpen, FabClose, FabRClockwise, FabRanticlockWise;
+    private FloatingActionButton fabMain, fabMsg, fabThumb;
+    boolean isOpen = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tab_layout_demo);
+
+
+        //플롯팅변수
+        FabOpen = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_open);
+        FabClose = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_close);
+        FabRClockwise = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rotate_clockwise);
+        FabRanticlockWise = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rotate_anticlockwise);
+
+        fabMain = (FloatingActionButton)findViewById(R.id.fabMain);
+        fabMsg = (FloatingActionButton)findViewById(R.id.fabMsg);
+        fabThumb = (FloatingActionButton)findViewById(R.id.fabThumb);
+        fabMain.setOnClickListener(clickListener);
+        fabMsg.setOnClickListener(clickListener);
+        fabThumb.setOnClickListener(clickListener);
+
+
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -65,6 +89,32 @@ public class TabLayoutDemoActivity extends AppCompatActivity {
 
 
     }
+
+    View.OnClickListener clickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.fabMain :
+                    if (!isOpen) {
+                        fabThumb.startAnimation(FabOpen);
+                        fabMsg.startAnimation(FabOpen);
+                        fabMain.startAnimation(FabRClockwise);
+                        fabThumb.setClickable(true);
+                        fabMsg.setClickable(true);
+                        isOpen = true;
+                    } else {
+                        fabThumb.startAnimation(FabClose);
+                        fabMsg.startAnimation(FabClose);
+                        fabMain.startAnimation(FabRanticlockWise);
+                        fabThumb.setClickable(false);
+                        fabMsg.setClickable(false);
+                        isOpen = false;
+                    }
+                    break;
+            }
+        }
+    };
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
